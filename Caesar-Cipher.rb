f13b1343635aa0d03ca7c0a7c1b38a5cc2991c74
Justin <jcow@pop-os.localdomain>
2021-09-25 22:38:47 +0900

    How exactly does the map function work?

--- Caesar-Cipher.rb
+++ Caesar-Cipher.rb
@@ -1,23 +1,31 @@
 require 'pry-byebug'
 
-def into_bytes (string)
+
+def encode(string, mod=2)
   b = string.bytes
-  b.each do |bite|
-    if bite == 32
-      p "space"
+  z = "z".bytes
+  a = "a".bytes
+  p b
+ # b.map! {|bit| bit != 32 ? bit + mod : bit}
+  b.map! do |bit|
+    if bit != 32
+      bit += mod
+      if bit > 122
+        start = 96
+        over = 122 - (bit + mod)
+        bit = start + over
+      end
+    else
+      bit = 32
     end
   end
   p b
+  p "#{z} is z and #{a} is a"
+  p z.pack('c*')
 end
 
-# def  encode(bytes)
-
-# end
-
 binding.pry
-
-into_bytes("apples")
-into_bytes("apples and oranges")
+encode("apples and oranges")
 
 
 
